1f5fa15598382e3ece3ded70ee601769b31cccbb
Yinnnn94 <rungina890904@gmail.com>
2026-07-29 09:18:53 +0800

    add invoice_type condition

--- DB_connect/connect_db.py
+++ DB_connect/connect_db.py
@@ -1,9 +1,9 @@
 from datetime import date, datetime
 from fastmcp import FastMCP
+import pandas as pd
 import pymysql
 import os
 import dotenv
-import pandas as pd
 
 server = FastMCP("Invoice_OCR_Project")
 dotenv.load_dotenv(dotenv_path=".env", override=True)
@@ -30,36 +30,44 @@ def get_time():
     return {"current_time": datetime.now().isoformat()}
 
 @server.tool(name="query_invoice_results_by_date_range", description="取得對應時間區間發票對帳結果")
-def query_invoice_results_by_date_range(start_date: str, end_date: str, result: str = None):
+def query_invoice_results_by_date_range(start_date: str, end_date: str, result: str = None, invoice_type: str = None):
+    """查詢特定月份、發票類型等資料
+    start_date: 初始月份
+    end_date: 結束月份
+    result: 想查詢的對帳結果
+    invoice_type: 發票類型
+    """
     conn = get_db_connection()
     try:
         # 建議加上 cursorclass，這樣回傳的 rows 才是字典格式，後面的 row[key] 才不會報錯
         with conn.cursor(pymysql.cursors.DictCursor) as cursor:
             # 1. 設定基礎 SQL
-            query = """SELECT * FROM n8n_result_with_TPE_Time 
+            query = """SELECT invoice_number, processed_at, seller_name , invoice_date, `result`, invoice_type FROM n8n_result_with_process_time 
                        WHERE invoice_date >= %s 
                        AND invoice_date <= %s
                        AND is_latest = 1"""
             params = [start_date, end_date]
-
             # 2. 動態增加 result 過濾條件
             if result is not None and str(result).strip() != "":
                 query += " AND result = %s"
                 params.append(result)
 
+            if invoice_type is not None and str(invoice_type).strip() != "":
+                query += " AND invoice_type = %s"
+                params.append(invoice_type)
+            print(query, params)
             cursor.execute(query, params)
             rows = cursor.fetchall()
-
             # 3. 轉換為 DataFrame
             df = pd.DataFrame(rows)
-
+            print(df.head())
             # 4. 日期序列化處理
             for col in df.columns:
                 if df[col].dtype == 'object':
                     df[col] = df[col].apply(
                         lambda x: x.isoformat() if isinstance(x, (date, datetime)) else x
                     )
-
+           # return {"db_data": df, "counts": len(df)}
             return {"db_data": df.to_dict('records'), "counts": len(df)}
     except Exception as e:
         return {"error": f"Database error: {str(e)}"}
